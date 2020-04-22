42b365d88775e47b9b30aac0e2ada57a798ce5f5
abdelich <sarminiabdel@gmail.com>
2020-04-22 10:42:02 +0300

    first commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 6.0f;
+
+    private Vector3 moveDirection;
+
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float deltaZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        moveDirection = new Vector3(deltaX, 0, deltaZ);
+
+        moveDirection = Vector3.ClampMagnitude(moveDirection, speed);
+        transform.Translate(moveDirection);
+    }
+}
---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    
+    public float gravity = 20f;
+    public float jumpSpeed = 8.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    private Rigidbody selfRigidbody;
+    private CharacterController character;
+
+    void Start()
+    {
+        selfRigidbody = GetComponent<Rigidbody>();
+        character = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            UpJump();
+    }
+
+    void UpJump()
+    {
+        if (ICanJump())
+        {
+            // Jump on ridigbody
+            GetComponent<Rigidbody>().AddForce(jumpSpeed * transform.up, ForceMode.Impulse);
+        }
+    }
+
+    bool ICanJump()
+    {
+        // Create Ray
+        Ray ray = new Ray(transform.position, transform.up * -1);
+
+        // Create Hit Info
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, transform.localScale.y + 0.2f))
+        {
+            return true;
+        }
+
+        // Nothing so return false
+        return false;
+    }
+
+}
